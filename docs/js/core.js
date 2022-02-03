5bc7bd7f46b071373bf418b619c8f4efbb7f2169
Vincent Morneau <vincent.morneau@oracle.com>
2022-02-03 14:04:41 -0500

    adjusted manifest structure to support app gallery

--- docs/js/core.js
+++ docs/js/core.js
@@ -1,56 +1,83 @@
-const stuck = "is-stuck",
-      headerContainer = document.querySelector( ".m-HeaderContainer" ),
-      apexVersionElement = document.getElementById( "apex_version" ),
-      apexVersionText = document.querySelector( ".m-VersionSwitcher-version" );
+( function () {
+    "use strict";
 
-function getURLParamValue( pParamName ) {
-    const queryString = window.location.search,
-          urlParams = new URLSearchParams( queryString );
-    return urlParams.get( pParamName );
-}
+    const stuck = "is-stuck",
+          headerContainer = document.querySelector( ".m-HeaderContainer" ),
+          apexVersionElement = document.getElementById( "apex_version" ),
+          apexVersionText = document.querySelector( ".m-VersionSwitcher-version" );
 
-function loadAPEXVersions() {
-    const apiURL = "https://api.github.com/repos/oracle/apex/branches",
-          versionURL = getURLParamValue( "version" );
+    let isAnchorShown = false;
 
-    const applyVersions = function ( pData ) {
-        const data = pData || [],
-              filteredData = data.filter( ( row ) => row.name !== "main" );
-        let option;
+    function getURLParamValue( pParamName ) {
+        const queryString = window.location.search,
+              urlParams = new URLSearchParams( queryString );
+        return urlParams.get( pParamName );
+    }
 
-        for ( let i = 0; i < filteredData.length; i++ ) {
-            if ( i === 0 ) {
-                option = new Option( "APEX " + filteredData[i].name, filteredData[i].name, true, false );
-            } else {
-                option = new Option( "APEX " + filteredData[i].name, filteredData[i].name, false, false );
+    function loadAPEXVersions() {
+        const apiURL = "https://api.github.com/repos/oracle/apex/branches",
+              versionURL = getURLParamValue( "version" );
+
+        const applyVersions = function ( pData ) {
+            const data = pData || [],
+                  filteredData = data.filter( ( row ) => row.name !== "main" );
+            let option;
+
+            // reverse loop, thus branches api is sorted by name, and we want the most current version to be first & default
+            for ( let i = filteredData.length - 1; i >= 0; i-- ) {
+                if ( i === 0 ) {
+                    option = new Option( "APEX " + filteredData[i].name, filteredData[i].name, true, false );
+                } else {
+                    option = new Option( "APEX " + filteredData[i].name, filteredData[i].name, false, false );
+                }
+                apexVersionElement.add( option );
             }
-            apexVersionElement.add( option );
-        }
 
-        if ( versionURL && apexVersionElement.querySelector( '[value="' + versionURL + '"]' ) ) {
-            apexVersionElement.value = versionURL;
-        }
-        apexVersionElement.dispatchEvent( new Event( "change" ) );
-    };
-    fetch( apiURL )
-        .then( ( response ) => response.json() )
-        .then( ( data ) => applyVersions( data ) );
-}
+            if ( versionURL && apexVersionElement.querySelector( '[value="' + versionURL + '"]' ) ) {
+                apexVersionElement.value = versionURL;
+            }
+            apexVersionElement.dispatchEvent( new Event( "change" ) );
+        };
+        fetch( apiURL )
+            .then( ( response ) => response.json() )
+            .then( ( data ) => applyVersions( data ) );
+    }
 
-function loadContent() {
-    const apexVersion = apexVersionElement.value,
-          manifestURL = "https://raw.githubusercontent.com/oracle/apex/" + apexVersion + "/docs/json/manifest.json";
+    function loadContent() {
+        const apexVersion = apexVersionElement.value,
+              manifestURL = "https://raw.githubusercontent.com/oracle/apex/" + apexVersion + "/docs/json/manifest.json";
 
-    const applyTemplate = function ( pData ) {
-        const data = pData || {};
+        const applyTemplate = function ( pData ) {
+            const data = pData || {};
 
-        const getTemplate = function ( pList, pButtonLabel ) {
-            const list = pList || [],
-                  buttonLabel = pButtonLabel || "Download App";
+            const prepareListData = function ( pList ) {
+                let list = pList || [],
+                    lang = "en";
 
-            return list
-                .map(
-                    ( entry ) => `<div class="u-Grid-row">
+                for ( let i = 0; i < list.length; i++ ) {
+                    let entry = list[i],
+                        newEntry = {};
+
+                    newEntry.name = entry.name ? entry.name : entry.i18n[lang] ? entry.i18n[lang].name : "";
+                    newEntry.description = entry.description ? entry.description : entry.i18n[lang] ? entry.i18n[lang].description : "";
+                    newEntry.slug = entry.slug ? entry.slug : entry.iconClass;
+                    newEntry.url = entry.url;
+                    newEntry.zip = entry.zip;
+                    newEntry.source = entry.source;
+
+                    list[i] = newEntry;
+                }
+
+                return list;
+            };
+
+            const getTemplate = function ( pList, pButtonLabel ) {
+                const list = pList || [],
+                      buttonLabel = pButtonLabel || "Download App";
+
+                return list
+                    .map(
+                        ( entry ) => `<div class="u-Grid-row">
                                     <div class="m-App">
                                         <div class="m-App-icon ${entry.slug}"></div>
                                         <div class="m-App-details">
@@ -62,49 +89,66 @@ function loadContent() {
                                         </div>
                                     </div>
                                 </div>`
-                )
-                .join( "" );
+                    )
+                    .join( "" );
+            };
+
+            document.getElementById( "samples" ).innerHTML = getTemplate( prepareListData( data.sampleApps ) );
+            document.getElementById( "apps" ).innerHTML = getTemplate( prepareListData( data.starterApps ) );
+            document.getElementById( "plug-ins" ).innerHTML = getTemplate( prepareListData( data.plugins ), "Download Plug-In" );
+
+            document.dispatchEvent( new Event( "remoteContentLoaded" ) );
         };
 
-        document.getElementById( "samples" ).innerHTML = getTemplate( data.sampleApps );
-        document.getElementById( "apps" ).innerHTML = getTemplate( data.starterApps );
-        document.getElementById( "plug-ins" ).innerHTML = getTemplate( data.plugins, "Download Plug-In" );
-    };
+        fetch( manifestURL )
+            .then( ( response ) => response.json() )
+            .then( ( data ) => applyTemplate( data ) );
+    }
 
-    fetch( manifestURL )
-        .then( ( response ) => response.json() )
-        .then( ( data ) => applyTemplate( data ) );
-}
-
-function toggleStuck() {
-    if ( window.pageYOffset > 0 ) {
-        if ( !headerContainer.classList.contains( stuck ) ) {
-            headerContainer.classList.add( stuck );
-        }
-    } else {
-        if ( headerContainer.classList.contains( stuck ) ) {
-            headerContainer.classList.remove( stuck );
+    function toggleStuck() {
+        if ( window.pageYOffset > 0 ) {
+            if ( !headerContainer.classList.contains( stuck ) ) {
+                headerContainer.classList.add( stuck );
+            }
+        } else {
+            if ( headerContainer.classList.contains( stuck ) ) {
+                headerContainer.classList.remove( stuck );
+            }
         }
     }
-}
 
-function updateVersionText() {
-    apexVersionText.textContent = apexVersionElement.value;
-}
+    function updateVersionText() {
+        apexVersionText.textContent = apexVersionElement.value;
+    }
 
-apexVersionElement.addEventListener( "change", function () {
-    updateVersionText();
-    loadContent();
-} );
+    function scrollToAnchor() {
+        let anchorElement;
+        if ( location.hash ) {
+            anchorElement = document.querySelector( location.hash );
+            anchorElement.scrollIntoView();
+        }
+    }
 
-window.addEventListener( "scroll", function () {
-    toggleStuck();
-} );
+    apexVersionElement.addEventListener( "change", function () {
+        updateVersionText();
+        loadContent();
+    } );
 
-document.addEventListener( "DOMContentLoaded", function ( event ) {
-    toggleStuck();
-} );
+    window.addEventListener( "scroll", function () {
+        toggleStuck();
+    } );
 
-( function () {
+    document.addEventListener( "DOMContentLoaded", function () {
+        toggleStuck();
+    } );
+
+    document.addEventListener( "remoteContentLoaded", function () {
+        if ( !isAnchorShown ) {
+            scrollToAnchor();
+            isAnchorShown = true;
+        }
+    } );
+
+    // load versions, which initiates loading content
     loadAPEXVersions();
 } )();
