6e10a1dd8f02f400a0e82d66f46b7c50db071983
Daniel Hochleitner <daniel.h.hochleitner@oracle.com>
2024-04-23 09:21:49 +0200

    update qAsk app

--- starter-apps/qask/qask.sql
+++ starter-apps/qask/qask.sql
@@ -123,7 +123,7 @@ wwv_imp_workspace.create_flow(
 ,p_public_user=>'APEX_PUBLIC_USER'
 ,p_proxy_server=>nvl(wwv_flow_application_install.get_proxy,'')
 ,p_no_proxy_domains=>nvl(wwv_flow_application_install.get_no_proxy_domains,'')
-,p_flow_version=>'Release 1.0'
+,p_flow_version=>'23.2.0'
 ,p_flow_status=>'AVAILABLE_W_EDIT_LINK'
 ,p_flow_unavailable_text=>'This application is currently unavailable at this time.'
 ,p_exact_substitutions_only=>'Y'
@@ -137,7 +137,7 @@ wwv_imp_workspace.create_flow(
 ,p_substitution_string_01=>'APP_NAME'
 ,p_substitution_value_01=>'qAsk'
 ,p_last_updated_by=>'DANIEL'
-,p_last_upd_yyyymmddhh24miss=>'20240423070207'
+,p_last_upd_yyyymmddhh24miss=>'20240423072006'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>24
 ,p_print_server_type=>'INSTANCE'
